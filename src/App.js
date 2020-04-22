e311cb2e4abb14232a22e9d48d8ad1611b226f6b
mayank.sisodiya <mayank.sisodiya@hashedin.com>
2020-04-22 21:02:00 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JitsiMeetComponent from './JitsiVideo';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ width: "100%", height: "100%" }} className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <JitsiMeetComponent></JitsiMeetComponent>
       </header>
     </div>
   );
